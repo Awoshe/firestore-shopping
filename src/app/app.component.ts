f6b2d7e3f1ecac5ee641d9ac130d13f3065fc20e
Jorge Vergara <javebratt@gmail.com>
2018-01-05 10:04:02 -0500

    Creates authentication provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = TabsPage;
+      } else {
+        this.rootPage = 'LoginPage';
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/providers/inventory/inventory.ts
+++ src/providers/inventory/inventory.ts
@@ -1,17 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-/*
-  Generated class for the InventoryProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class InventoryProvider {
-
-  constructor(public http: HttpClient) {
-    console.log('Hello InventoryProvider Provider');
+  userId: string;
+  constructor(public afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userId = user.uid;
+      }
+    });
   }
 
+  async getTeamId(): Promise<string> {
+    const userProfile: firebase.firestore.DocumentSnapshot = await firebase
+      .firestore()
+      .doc(`userProfile/${this.userId}`)
+      .get();
+
+    return userProfile.data().teamId;
+  }
 }
